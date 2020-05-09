e64682bd1cdc6d3bf15abcdf5851f5d5bd7860f5
Nicholas Lantz <nlantz@pm.me>
2020-05-08 22:16:48 -0600

    Solved RNA problem.

--- soln.lisp
+++ soln.lisp
@@ -4,3 +4,6 @@
   (format stream "~a ~a ~a ~a"
 	  (count #\A str) (count #\C str)
 	  (count #\G str) (count #\T str)))
+
+(defun rna (str &optional (stream *standard-output*))
+  (format stream "~s" (substitute #\U #\T str)))
