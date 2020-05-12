3e5e3e1521e5c486e39040b6803650eb31c8e750
Nicholas Lantz <nlantz@pm.me>
2020-05-12 09:12:12 -0600

    Added protein functions to dna.lisp

--- lib/dna.lisp
+++ lib/dna.lisp
@@ -1,5 +1,36 @@
 (in-package #:rosalind)
 
+(defparameter *rna-codon-table*
+  '((UUU . F)      (CUU . L)      (AUU . I)      (GUU . V)
+    (UUC . F)      (CUC . L)      (AUC . I)      (GUC . V)
+    (UUA . L)      (CUA . L)      (AUA . I)      (GUA . V)
+    (UUG . L)      (CUG . L)      (AUG . M)      (GUG . V)
+    (UCU . S)      (CCU . P)      (ACU . T)      (GCU . A)
+    (UCC . S)      (CCC . P)      (ACC . T)      (GCC . A)
+    (UCA . S)      (CCA . P)      (ACA . T)      (GCA . A)
+    (UCG . S)      (CCG . P)      (ACG . T)      (GCG . A)
+    (UAU . Y)      (CAU . H)      (AAU . N)      (GAU . D)
+    (UAC . Y)      (CAC . H)      (AAC . N)      (GAC . D)
+    (UAA . Stop)   (CAA . Q)      (AAA . K)      (GAA . E)
+    (UAG . Stop)   (CAG . Q)      (AAG . K)      (GAG . E)
+    (UGU . C)      (CGU . R)      (AGU . S)      (GGU . G)
+    (UGC . C)      (CGC . R)      (AGC . S)      (GGC . G)
+    (UGA . Stop)   (CGA . R)      (AGA . R)      (GGA . G)
+    (UGG . W)      (CGG . R)      (AGG . R)      (GGG . G)))
+
+(defun codon->amino-acid (codon)
+  (assocdr (intern codon) *rna-codon-table*))
+
+(defun rna->protein (str &key (start 0) stop (protein nil))
+  (if (or (>= start (length str)) (and stop (>= start stop)))
+      (nreverse protein)
+      (rna->protein str
+		    :start (+ start 3) :stop stop
+		    :protein (cons (codon->amino-acid (subseq str start (+ start 3))) protein))))
+
+(defun protein->str (prot)
+  (apply #'concat (mapcar #'symbol-name (loop for p in prot until (eq p 'stop) collect p))))
+
 (let ((complements '((#\A . #\T) (#\T . #\A)
 		     (#\C . #\G) (#\G . #\C))))
   (defun dna-complement (str &optional (start 0) end (acc (make-string (length str))))
