ceed20838910a7b6441cfed51e74a896a8ee0a75
Nicholas Lantz <nlantz@pm.me>
2020-05-18 11:25:53 -0600

    Removed test parameter from trie.
    I don't think we'll use tries for anything but strings, so it seems
    unecessary.

--- lib/trie.lisp
+++ lib/trie.lisp
@@ -1,12 +1,10 @@
+(in-package #:rosalind)
 
+;; TODO: Add constructor that takes strings, build trie out of those
 (defclass trie ()
   ((nnodes
     :initform 1
     :reader trie-nnodes)
-   (test
-    :initarg :test
-    :accessor trie-test
-    :initform #'eql)
    (adjacencies
     :reader trie-adjacencies
     :initform (make-array 1
@@ -26,13 +24,13 @@
 	  (1- (incf nnodes))))))
 
 (defmethod edgep ((tr trie) parent data)
-  (with-slots (test adjacencies) tr
-    (rassoc data (aref adjacencies parent) :test test)))
+  (with-slots (adjacencies) tr
+    (rassoc data (aref adjacencies parent))))
 
 (defmethod add-string ((tr trie) (str string) &key (parent 0) (start 0) end)
   (if (or (= start (length str)) (and end (= start end)))
       str
-      (with-slots (nnodes test adjacencies) tr
+      (with-slots (nnodes adjacencies) tr
 	(if-let ((entry (edgep tr parent (elt str start))))
 	    (add-string tr str :parent (car entry) :start (1+ start))
 	    (let ((id (add-edge tr parent (elt str start))))
