81aef4fc04cfd3f949c1f675e12a73448eebe2d4
Nicholas Lantz <nlantz@pm.me>
2020-05-18 10:58:30 -0600

    Added basic trie construction.

---
+++ lib/trie.lisp
@@ -0,0 +1,43 @@
+
+(defclass trie ()
+  ((nnodes
+    :initform 1
+    :reader trie-nnodes)
+   (test
+    :initarg :test
+    :accessor trie-test
+    :initform #'eql)
+   (adjacencies
+    :reader trie-adjacencies
+    :initform (make-array 1
+			  :element-type 'list
+			  :initial-contents '(())
+			  :adjustable t
+			  :fill-pointer t))))
+
+(defmethod add-edge ((tr trie) parent data)
+  (with-slots (nnodes adjacencies) tr
+    (assert (< parent nnodes))
+    (if (edgep tr parent data)
+	nil
+	(progn
+	  (push (cons nnodes data) (aref adjacencies parent))
+	  (vector-push-extend nil adjacencies)
+	  (1- (incf nnodes))))))
+
+(defmethod edgep ((tr trie) parent data)
+  (with-slots (test adjacencies) tr
+    (rassoc data (aref adjacencies parent) :test test)))
+
+(defmethod add-string ((tr trie) (str string) &key (parent 0) (start 0) end)
+  (if (or (= start (length str)) (and end (= start end)))
+      str
+      (with-slots (nnodes test adjacencies) tr
+	(if-let ((entry (edgep tr parent (elt str start))))
+	    (add-string tr str :parent (car entry) :start (1+ start))
+	    (let ((id (add-edge tr parent (elt str start))))
+	      (add-string tr str :parent id :start (1+ start)))))))
+
+(defmethod pattern-match ((str string) (tr trie))
+  (with-slots (adjacencies) tr
+    
